f6fd88af53764106e72a05b27cb95b945f49437f
IvonaKovacevic <102519849+IvonaKovacevic@users.noreply.github.com>
2022-07-14 04:17:22 -0400

    Es 1 / GIUSTO

--- esgiovedi.c
+++ esgiovedi.c
@@ -5,23 +5,19 @@ int main()
 
 {
 
-int i;
-int k;
-int x;
-int y;
+int numero;
+int incremento;
+int decremento;
 
 
-printf("Inserisci valore da incrementare i");
-scanf("%d", &i);
+printf("Inserisci valore da incrementare numero");
+scanf("%d", &numero);
 
-printf("Inserisci valore da decrementare k");
-scanf("%d",&k);
+incremento=numero+1;
+printf("Incrementato %d\n",numero+1);
 
-x=i++;
-printf("i Incrementato %d\n",i);
-
-y=k--;
-printf("Decrementa valore %d\n",k);
+decremento=numero-1;
+printf("Valore decrementato %d\n",numero-1);
 
 
 
